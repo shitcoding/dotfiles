e61c542360c275b051d6b1b5afc971763a86b53f
shitcoding <shitcoder@pm.me>
2022-01-16 16:08:20 +0300

    add installation script and makefile

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Copy dotfiles to appropriate locations
+cp ./alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp ./tmux/.tmux.conf ~/.tmux.conf
+cp ./vim/.vimrc ~/.vimrc
+cp ./zsh/.zshrc ~/.zshrc
+cp ./powerlevel10k/.p10k.zsh ~/.p10k.zsh
