5aa4dd92992f7b9a47ddfae21cc7195b3e9ad6fe
shitcoding <shitcoder@pm.me>
2022-09-13 18:20:41 +0300

    add nvim-colorizer plugin for hex colors highlighting

--- lvim/config.lua
+++ lvim/config.lua
@@ -353,6 +353,20 @@ lvim.plugins = {
     end,
   },
   { "folke/trouble.nvim" },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript", "tmux" }, {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
 }
 
 
